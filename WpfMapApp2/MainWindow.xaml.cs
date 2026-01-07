b48142ccb1d6ea45f6e788c6bf7bf5f3d507b9ca
caps12345 <1946214508@qq.com>
2026-01-07 15:31:36 +0800

    1

--- WpfMapApp2/MainWindow.xaml.cs
+++ WpfMapApp2/MainWindow.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
-using Esri.ArcGISRuntime.Geometry;
-using Esri.ArcGISRuntime.Mapping;
-using Esri.ArcGISRuntime.UI;
+﻿using System.Windows;
 
 namespace WpfMapApp2
 {
@@ -12,23 +7,6 @@ namespace WpfMapApp2
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private async void MapView_Loaded(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (MapView?.Map != null)
-                {
-                    // 设置初始视点到江苏省范围
-                    var jiangsuExtent = new Envelope(116.0, 30.0, 122.0, 35.0, SpatialReferences.Wgs84);
-                    await MapView.SetViewpointAsync(new Viewpoint(jiangsuExtent));
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"地图初始化失败: {ex.Message}", "错误");
-            }
         }
     }
 }
