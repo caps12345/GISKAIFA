9b38eedff0a49fa1acda4846e9366257aa452956
McCain012 <a0910171156@gmail.com>
2026-01-12 18:02:07 +0800

    Add files via upload

---
+++ F5/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI;
+using System;
+using System.Windows;
+
+namespace WpfMapApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // 初始化 ViewModel 并赋值给 DataContext
+            var vm = new MapViewModel();
+            this.DataContext = vm;
+
+            // 订阅跳转事件
+            vm.RequestNavigation += (s, e) =>
+            {
+                Dispatcher.Invoke(async () =>
+                {
+                    if (e.IsDistrictZoom && e.DistrictEnvelope != null)
+                    {
+                        await MapView.SetViewpointGeometryAsync(e.DistrictEnvelope, 20);
+                    }
+                    else if (e.Center != null)
+                    {
+                        await MapView.SetViewpointCenterAsync(e.Center, e.Scale);
+                    }
+                });
+            };
+        }
+    }
+}
